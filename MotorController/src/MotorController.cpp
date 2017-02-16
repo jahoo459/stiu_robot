98bfe6061cb5eb789f0689625eb14b97f9750d0a
jasiek <jasiek@jasiek-ThinkPad-T510>
2017-02-16 22:16:30 +0100

    MotorController added

---
+++ MotorController/src/MotorController.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MotorController.cpp
+// Author      : Jan Haberny
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
