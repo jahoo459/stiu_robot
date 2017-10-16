980cff19daaf92d5fc39aea9c088b28577602405
Olga <olga.kasperek@outlook.com>
2017-10-16 18:59:28 +0200

    modyfy the state_publisher to correctly calculate wheels velocity

--- stiu_setup_tf/src/state_publisher.cpp
+++ stiu_setup_tf/src/state_publisher.cpp
@@ -3,6 +3,7 @@
 #include <sensor_msgs/JointState.h>
 #include <tf/transform_broadcaster.h>
 #include <movement_control/Omegas.h>
+#include <nav_msgs/Odometry.h>
 
 double r_wheel_vel = 0;
 double l_wheel_vel = 0;
@@ -25,32 +26,39 @@ int main(int argc, char** argvs)
     std::cout << "START:" << std::endl;
     
     // robot state
-    double right_wheel_joint=0, left_wheel_joint=0, angle=M_PI, f_grabtime = 0, x = 0, y = 0;
+    double right_wheel_joint=0, left_wheel_joint=0, angle = 0, dist_r = 0, dist_l = 0, dr = 0, da = 0;
     const double degree = M_PI/180;
     double r = 0.0325;
+    double wheel_sep = 0.05;
 
     joint_pub = new ros::Publisher(n.advertise<sensor_msgs::JointState>("joint_states", 1));
     ros::Subscriber sub = n.subscribe("omegas", 1000, callback);
-
-    //ros::NodeHandle n;
-    joint_pub = new ros::Publisher(n.advertise<sensor_msgs::JointState>("joint_states", 1));    
+    
     tf::TransformBroadcaster broadcaster;
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(1);
 
     // message declarations
-    geometry_msgs::TransformStamped odom_trans;
+    
     sensor_msgs::JointState joint_state;
-    odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "base_link";
-    f_grabtime = ros::Time::now().toSec();
-   
+  
+
+    ros::Time current_time, last_time;
+    current_time = ros::Time::now();
+    last_time = ros::Time::now();
 
     while (ros::ok()) {
-        //update joint_state
+     
 	ros::spinOnce();
-	f_grabtime = ros::Time::now().toSec();
-	std::cout << "f_grabtime:" << f_grabtime << "\n"<< std::endl;
-        joint_state.header.stamp = ros::Time::now();
+
+	current_time = ros::Time::now();
+	double dt = (current_time - last_time).toSec();
+
+	geometry_msgs::TransformStamped odom_trans;
+	odom_trans.header.stamp = current_time;
+	odom_trans.header.frame_id = "odom";
+	odom_trans.child_frame_id = "base_link";
+
+        joint_state.header.stamp = current_time;
         joint_state.name.resize(2);
         joint_state.position.resize(2);
         joint_state.name[0] ="right_wheel_joint";
@@ -58,30 +66,42 @@ int main(int argc, char** argvs)
         joint_state.name[1] ="left_wheel_joint";
         joint_state.position[1] = left_wheel_joint;
 
-	x = r_wheel_vel/1000 * f_grabtime/1000;
-	y = l_wheel_vel/1000 * f_grabtime/1000;
+	dist_r = dt * r / 2 * r_wheel_vel/10; 
+	dist_l =  dt * r / 2 * l_wheel_vel/10;
 
-	std::cout << "x:" << x << ", y: " << y << "\n" << std::endl;
-        // update transform
-        // (moving in a circle with radius=2)
-        odom_trans.header.stamp = ros::Time::now();
-        odom_trans.transform.translation.x = x;
-        odom_trans.transform.translation.y = 0;
-        odom_trans.transform.translation.z = 0;
-        odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(0);
+	dr = (dist_r + dist_l) / 2;
+	da = (dist_r - dist_l) / wheel_sep;
+
+	std::cout <<"Data: "<< da << " \n"<<std::endl;
+
+	if (da!=0)
+	{
+	    angle = da + M_PI/2;
+	}
+	else
+	{
+	    angle = da;
+	}
+
+
+	// update transform
+        odom_trans.transform.translation.x = -dr * cos(da);
+        odom_trans.transform.translation.y = -dr * sin(da);
+        odom_trans.transform.translation.z = 0.0;
+        odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(angle);
+
 
         //send the joint state and transform
         joint_pub->publish(joint_state);
         broadcaster.sendTransform(odom_trans);
       
-        right_wheel_joint = x/(2*M_PI*r) * 360;
-	left_wheel_joint = y/(2*M_PI*r) * 360;
-	//loop_rate.sleep();
+        right_wheel_joint = dist_l/(2*M_PI*r)*360;
+	left_wheel_joint = dist_r/(2*M_PI*r)*360;
 
-
+	loop_rate.sleep();
 }
    
-    ros::spin();
+    //ros::spin();
     delete joint_pub;
 
     return 0;
